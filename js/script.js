705aa242d65d1c391b59829236b1f6fe796fa8af
Louis Gabriel <louis.gabriel@orange.fr>
2018-12-02 16:10:05 +0100

    Sidenav now closes on click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   // Init Materialize CSS Components
-  $(".sidenav").sidenav({
-    menuWidth: 100
-  });
+  $(".sidenav").sidenav();
   $(".parallax").parallax();
 });
